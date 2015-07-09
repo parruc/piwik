ebd48591b4c3234c9dca728c83f4c9c7ec07cbc6
Matteo Parrucci <parruc@gmail.com>
2015-07-09 16:29:12 +0200

    removed comments now that we use git to version

--- plugins/DashboardSWD/javascripts/plugin.js
+++ plugins/DashboardSWD/javascripts/plugin.js
@@ -63,55 +63,6 @@ $(document).ready(function () {
         // BBB piwik uses a striped down version of bootstrap so this datepicker shows up and is ugly
         $(".dashboard-swd .daterangepicker").hide();
 
-// BBB Patch per modifica su js originale piwik
-/*
---- plugins/CoreVisualizations/javascripts/jqplotEvolutionGraph.js.orig	2015-06-22 17:11:37.000000000 +0200
-+++ plugins/CoreVisualizations/javascripts/jqplotEvolutionGraph.js	2015-06-22 17:09:30.000000000 +0200
-@@ -87,6 +87,9 @@
-                     JqplotGraphDataTablePrototype._destroyDataPointTooltip.call(this, $(this));
-                 })
-                 .on('jqplotClick', function (e, s, i, d) {
-+                    if($(".dashboard-swd").length > 0 ){
-+                        return;
-+                    }
-                     if (lastTick !== false && typeof self.jqplotParams.axes.xaxis.onclick != 'undefined'
-                         && typeof self.jqplotParams.axes.xaxis.onclick[lastTick] == 'string') {
-                         var url = self.jqplotParams.axes.xaxis.onclick[lastTick];
-
-	console.log($("#dataTable_0Chart").length);
-        $(document).on("jqplotClick", "#dataTable_0Chart", function(evt){
-            console.log("clicked");
-            evt.preventDefault();
-            evt.stopPropagation();
-        });*/
-
-
-// BBB Patch per modifica su js originale piwik
-/*
---- /var/www/piwik/plugins/UserCountryMap/javascripts/visitor-map-old.js        2015-07-01 16:01:38.417382171 +0200
-+++ /var/www/piwik/plugins/UserCountryMap/javascripts/visitor-map.js    2015-07-01 16:22:41.328704707 +0200
-@@ -766,6 +766,9 @@
-                                     return '<h3>' + data.name + '</h3>' +
-                                         formatValueForTooltips(region, metric, iso);
-                                 }).on('click',function (d, path, evt) {
-+                                    if ($(".dashboard-swd").length > 0){
-+                                        return
-+                                    }
-                                     var region = regionDict[regionCode(d)];
-                                     if (region && region.label) {
-                                         if (evt.shiftKey) {
-@@ -937,6 +940,9 @@
-                                     }
-                                 },
-                                 click: function (city, symbol, evt) {
-+                                    if ($(".dashboard-swd").length > 0){
-+                                        return
-+                                    }
-                                     if (evt.shiftKey) {
-                                         addMultipleRowEvolution('getCity', city.label);
-                                         symbol.path.attr('fill', citySelectedColor);
-*/
-
 
         if (document.getElementById('liveReportContainer')) {
 
